870c1456baece86ba360d39dbb73bfab5d38ecad
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-13 17:37:14 +0200

    added second class

---
+++ Module3HW3/Module3HW3/SecondClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Module3HW3
+{
+    public class SecondClass
+    {
+        private int _result;
+
+        public Func<int, bool> Calc(Func<int, int, int> func, int x, int y)
+        {
+            _result = func.Invoke(x, y);
+            return Result;
+        }
+
+        public bool Result(int x)
+        {
+            var num = _result % x;
+            return num % 2 == 0;
+        }
+    }
+}
