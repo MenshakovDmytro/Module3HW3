70d4f72f462a1aa2155c5b51ef204b522743db48
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-13 17:37:06 +0200

    added first class

---
+++ Module3HW3/Module3HW3/FirstClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Module3HW3
+{
+    public class FirstClass
+    {
+        public Action<bool> Action { get; set; }
+
+        public int Pow(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
