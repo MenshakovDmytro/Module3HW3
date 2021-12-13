546cc4998017f0fd2979421aac378f110b2643a6
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-13 17:37:24 +0200

    added program

--- Module3HW3/Module3HW3/Program.cs
+++ Module3HW3/Module3HW3/Program.cs
@@ -6,7 +6,16 @@ namespace Module3HW3
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var program = new Program();
+            var firstClass = new FirstClass() { Action = program.Show };
+            var secondClass = new SecondClass();
+            var result = secondClass.Calc(firstClass.Pow, 6, 7).Invoke(7);
+            firstClass.Action.Invoke(result);
+        }
+
+        public void Show(bool result)
+        {
+            Console.WriteLine(result);
         }
     }
 }
